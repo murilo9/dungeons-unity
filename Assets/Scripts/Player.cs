a5154a98e8b179d261e323ef9aa117afbdb494a0
Murilo Henrique Matias <murilomatias@alunos.utfpr.edu.br>
2019-09-03 04:34:53 -0300

    Player sprite flip on move

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,12 +10,14 @@ public class Player : MonoBehaviour
     private Rigidbody2D rigidBody;
     private Vector3 change;
     private Animator animator;
+    private SpriteRenderer renderer;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        renderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -24,18 +26,22 @@ public class Player : MonoBehaviour
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
+        if (change.x == -1)
+            renderer.flipX = true;
+        else if (change.x == 1)
+            renderer.flipX = false;
         if(change != Vector3.zero && canMove)       //Check if movement key is pressed
         {
-            if (!moving)
+            if (!moving)    //Start walk animation
             {
                 moving = true;
-                animator.SetTrigger("doWalk");      //Start walk animation
+                animator.SetTrigger("doWalk");      
             }
             MoveCharacter();    //Move on space
         }
         else        //If no movement key is pressed
         {
-            if (moving)
+            if (moving)     //Stop walk animation
             {
                 moving = false;
                 animator.SetTrigger("stopWalk");
